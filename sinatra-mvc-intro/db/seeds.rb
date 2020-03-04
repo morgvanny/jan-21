8123f425fdd87a4255247c5a380d0436ba452129
Morgan VanYperen <morgvanny@gmail.com>
2020-03-03 19:37:09 -0800

    finish mvc lesson

--- sinatra-mvc-intro/db/seeds.rb
+++ sinatra-mvc-intro/db/seeds.rb
@@ -1,3 +1,5 @@
 Book.destroy_all
 
-GoogleBooks::Adapter.new("Madeline L'Engle").fetch_books
+GoogleBooks::Adapter.new("Bernie Sanders").fetch_books 
+GoogleBooks::Adapter.new("Thomas Piketty").fetch_books 
+GoogleBooks::Adapter.new("Karl Marx").fetch_books 
